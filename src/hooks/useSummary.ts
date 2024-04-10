6d1cc090452316d61869009614e3c335cc59d47a
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2024-04-10 18:30:49 -0300

    feat: 🎸 finalizacaoo do projeto dtmoney com usememo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,28 +1,31 @@
+import { useMemo } from "react";
 import { TransactionContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
 export function useSummary() {
-  const transactions  = useContextSelector(TransactionContext, (context) => {
+  const transactions = useContextSelector(TransactionContext, (context) => {
     return context.transactions;
   });
 
-  const summary = transactions.reduce(
-    (acc, transactions) => {
-      if (transactions.type === "income") {
-        acc.income += transactions.price;
-        acc.total += transactions.price;
-      } else {
-        acc.outcome += transactions.price;
-        acc.total -= transactions.price;
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transactions) => {
+        if (transactions.type === "income") {
+          acc.income += transactions.price;
+          acc.total += transactions.price;
+        } else {
+          acc.outcome += transactions.price;
+          acc.total -= transactions.price;
+        }
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    );
+  }, [transactions]);
   return summary;
 }
